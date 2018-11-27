43612d4965c6e97975f37a0250f25dd9ab842f27
reang <duereang@gmail.com>
2018-11-28 00:46:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const request = require('request')
 const port = process.env.PORT || 3030
 
 http.createServer((req, res) => {
-	//req.pipe(request(req.headers.referer)).pipe(res)
+	req.pipe(request(req.headers.referer)).pipe(res)
 	
-	try {
-		req.pipe(request(req.headers.referer, {gzip: true, json: true})).on('error', err => {
-			res.writeHead(200, {'Content-Type': 'text/plain'})
-			res.end('{}')
-		}).pipe(res)
-	} catch(err) {
-		res.writeHead(200, {'Content-Type': 'text/plain'})
-		res.end('{}')
-	}
+// 	try {
+// 		req.pipe(request(req.headers.referer, {gzip: true, json: true})).on('error', err => {
+// 			res.writeHead(200, {'Content-Type': 'text/plain'})
+// 			res.end('{}')
+// 		}).pipe(res)
+// 	} catch(err) {
+// 		res.writeHead(200, {'Content-Type': 'text/plain'})
+// 		res.end('{}')
+// 	}
 
 }).listen(port, () => {
 	console.log('Proxy started on port ' + port)
