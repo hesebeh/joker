c1e651900e8b816cdad72b8c8af78a9cf06cb072
reang <duereang@gmail.com>
2018-11-27 17:22:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const http = require('http')
+const request = require('request')
+const port = process.env.PORT || 3030
+
+http.createServer((req, res) => {
+	//req.pipe(request(req.headers.referer)).pipe(res)
+	
+	try {
+		req.pipe(request(req.headers.referer)).on('error', err => {
+			res.writeHead(200, {'Content-Type': 'text/plain'})
+			res.end('{}')
+		}).pipe(res)
+	} catch(err) {
+		res.writeHead(200, {'Content-Type': 'text/plain'})
+		res.end('{}')
+	}
+
+}).listen(port, () => {
+	console.log('Proxy started on port ' + port)
+});
